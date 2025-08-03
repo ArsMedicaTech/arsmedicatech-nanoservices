00e18b0506aa90fc3c9a12162f37d20f21251697
darren277 <darren277@yahoo.com>
2025-08-03 06:27:49 -0700

    General gRPC controller.

---
+++ amt_nano/services/grpc_service.py
@@ -0,0 +1,21 @@
+""""""
+from typing import Optional, Type
+
+import grpc
+
+
+class GRPCController:
+    def __init__(self, stub_class: Type, address: str, secure: bool = False, cert_path: Optional[str] = None):
+        if secure:
+            credentials = grpc.ssl_channel_credentials(
+                root_certificates=open(cert_path, 'rb').read() if cert_path else None
+            )
+            self.channel = grpc.secure_channel(address, credentials)
+        else:
+            self.channel = grpc.insecure_channel(address)
+
+        self.stub = stub_class(self.channel)
+
+    def call(self, method_name: str, request_obj):
+        method = getattr(self.stub, method_name)
+        return method(request_obj)

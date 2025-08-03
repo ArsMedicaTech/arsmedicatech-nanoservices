35c12dcff96b2314ec274ca9e57d7d6c2d96afb2
darren277 <darren277@yahoo.com>
2025-08-03 06:31:26 -0700

    Fixed some MyPy issues.

--- amt_nano/services/grpc_service.py
+++ amt_nano/services/grpc_service.py
@@ -1,11 +1,11 @@
 """"""
-from typing import Optional, Type
+from typing import Any, Optional, Type
 
 import grpc
 
 
 class GRPCController:
-    def __init__(self, stub_class: Type, address: str, secure: bool = False, cert_path: Optional[str] = None):
+    def __init__(self, stub_class: Type[Any], address: str, secure: bool = False, cert_path: Optional[str] = None):
         if secure:
             credentials = grpc.ssl_channel_credentials(
                 root_certificates=open(cert_path, 'rb').read() if cert_path else None
@@ -16,6 +16,6 @@ class GRPCController:
 
         self.stub = stub_class(self.channel)
 
-    def call(self, method_name: str, request_obj):
+    def call(self, method_name: str, request_obj: Any) -> Any:
         method = getattr(self.stub, method_name)
         return method(request_obj)

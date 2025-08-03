c07da5fec94ccaee6f63eab5ebbf7b096de75d1f
darren277 <darren277@yahoo.com>
2025-08-03 06:31:43 -0700

    Patient controller.

--- amt_nano/services/grpc_service.py
+++ amt_nano/services/grpc_service.py
@@ -19,3 +19,29 @@ class GRPCController:
     def call(self, method_name: str, request_obj: Any) -> Any:
         method = getattr(self.stub, method_name)
         return method(request_obj)
+
+#from your_grpc_proto_pb2 import (CreatePatientRequest, DeletePatientRequest, GetPatientRequest, UpdatePatientRequest)
+#from your_grpc_proto_pb2_grpc import PatientServiceStub
+
+from amt_nano.proto.google.fhir.proto.r5.core.resources import patient_pb2
+
+
+class PatientController(GRPCController):
+    def __init__(self, address: str = "localhost:50051", secure: bool = False, cert_path: Optional[str] = None):
+        super().__init__(PatientServiceStub, address, secure, cert_path)
+
+    def create_patient(self, patient: patient_pb2.Patient):
+        request = CreatePatientRequest(resource=patient)
+        return self.call("CreatePatient", request)
+
+    def get_patient(self, patient_id: str):
+        request = GetPatientRequest(id=patient_id)
+        return self.call("GetPatient", request)
+
+    def update_patient(self, patient: patient_pb2.Patient):
+        request = UpdatePatientRequest(resource=patient)
+        return self.call("UpdatePatient", request)
+
+    def delete_patient(self, patient_id: str):
+        request = DeletePatientRequest(id=patient_id)
+        return self.call("DeletePatient", request)

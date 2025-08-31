ce1e1c0d0858ecf5f2cd4011f451a90f90d2ada7
darren277 <darren277@yahoo.com>
2025-08-30 19:21:04 -0700

    Custom logger class.

--- settings.py
+++ settings.py
@@ -5,8 +5,12 @@ from os.path import dirname, join
 
 from dotenv import load_dotenv
 
+from amt_nano.logger import Logger
+
 dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
+
+logger = Logger()
 
 
 GRPC_URL = os.environ.get("GRPC_URL", "localhost:50051")
